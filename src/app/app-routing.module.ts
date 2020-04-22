2ec0a7064be106d70d12563b0809ff5738997994
Lindsey Urquhart <lindsey.urquhart@gmail.com>
2020-04-22 14:59:51 -0400

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  },
+  {
+    path: 'tab6',
+    loadChildren: () => import('./tab6/tab6.module').then( m => m.Tab6PageModule)
+  },
+  {
+    path: 'tab7',
+    loadChildren: () => import('./tab7/tab7.module').then( m => m.Tab7PageModule)
+  },
+  {
+    path: 'tab8',
+    loadChildren: () => import('./tab8/tab8.module').then( m => m.Tab8PageModule)
+  },
+  // {
+  //   path: 'tab5',
+  //   loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
+  // },
+];
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
